228334664254795880ff17e77d57388d4fc2019d
Mustafa Siddiqui <mustafa.ar.siddiqui@gmail.com>
2020-03-31 20:59:42 +0530

    _

--- task_list/src/App.js
+++ task_list/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ButtonFunctionality from './modules/ButtonFunctionality'
+import ButtonFunctionality from './modules/ButtonFuctionality'
+import Table from './modules/Table'
 
 let App = () => 
     <div id="main container">
         <h1 id="title">Listify</h1>
+        <Table/>
         <ButtonFunctionality/>
     </div>
 export default App;
